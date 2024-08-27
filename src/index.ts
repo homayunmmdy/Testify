2a90281cd2b16b167d0cced65fdc62a2626f7f37
Homayoun <homayoun763@gmail.com>
2024-08-27 23:37:00 +0330

    add: click functionality

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,11 @@
-console.log("Let's Get Started")
+const imageElement = document.getElementById("imageElement") as HTMLImageElement;
+const coinElement = document.getElementById("coin") as HTMLParagraphElement;
+
+let coin = 0;
+
+if (imageElement) {
+    imageElement.addEventListener("click", () => {
+    coin += 1;
+    coinElement.textContent = `Coin : ${coin}`;
+  });
+}
