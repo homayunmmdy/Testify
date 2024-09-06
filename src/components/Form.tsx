a7896bd1f436dd424f70a7dc9bd6613a4ccc4288
Homayoun <homayoun763@gmail.com>
2024-09-06 23:30:59 +0330

    add show toast for form

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,24 +1,41 @@
+"use client";
 import { CountriesType } from "@/type/entities";
-import React from "react";
+import React, { useState } from "react";
+import toast, { Toaster } from "react-hot-toast";
 import { Countries } from "../data/staticData";
 
 const Form: React.FC = () => {
+  const [age, setAge] = useState(0);
+  const [name, setName] = useState("");
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+
+    if (age < 18 || age > 99) {
+      toast.error("you're age must be between 18 to 99 year's old");
+      return
+    }
+
+    toast.success(`Your're welcome ${name}`)
+  };
   return (
     <>
+      <Toaster />
       <p className="font-bold text-center text-xl mb-5">Personal Information</p>
-      <form className="flex flex-col">
+      <form className="flex flex-col" onSubmit={handleSubmit}>
         <input
           type="text"
+          value={name}
           placeholder="please enter your name"
           required
           autoFocus
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setName(e.target.value)}
           className="input input-bordered mb-4"
         />
         <input
           className="input input-bordered mb-4"
           type="number"
-          min={18}
-          max={99}
+          value={age}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => setAge(e.target.value as any)}
           placeholder="Please Enter your age"
           required
         />
@@ -26,7 +43,10 @@ const Form: React.FC = () => {
           className="textarea textarea-bordered mb-5"
           placeholder="Tell us about your self a little bit"
         ></textarea>
-        <select aria-label="combobox" className="select select-bordered w-full max-w-xs mb-5">
+        <select
+          aria-label="combobox"
+          className="select select-bordered w-full max-w-xs mb-5"
+        >
           <option disabled selected>
             Please select your country
           </option>
@@ -36,11 +56,11 @@ const Form: React.FC = () => {
         </select>
         <label data-testid="remember" className="label cursor-pointer">
           <span className="label-text">Remember me</span>
-          <input  type="checkbox" className="toggle" defaultChecked />
+          <input type="checkbox" className="toggle" defaultChecked />
         </label>
         <label data-testid="privaicyPolicy" className="label cursor-pointer">
           <span className="label-text">Agree with Privacy and Policy</span>
-          <input  required type="checkbox" defaultChecked className="checkbox" />
+          <input required type="checkbox" defaultChecked className="checkbox" />
         </label>
         <button type="submit" className="btn btn-primary">
           Submit
